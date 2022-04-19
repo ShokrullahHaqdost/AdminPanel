d59e65fd60dfad3af1c0222da666869f783c05a4
Arash <shokrullahhaqdost87@gmail.com>
2022-04-19 22:08:13 +0500

    complete the project

--- js/panel.js
+++ js/panel.js
@@ -72,31 +72,3 @@ function active_user_name_arrow(flag) {
     imgNameBox.style.justifyContent = "center";
   }
 }
-
-var ctx = document.getElementById("myChart").getContext("2d");
-      var myChart = new Chart(ctx, {
-        type: 'polarArea',
-        data: {
-          labels: ["هندزفری", "هدفون", "کیف هندزفری", "محافظ", "شارژر "],
-          datasets: [
-            {
-              label: "",
-              data: [12, 19, 3, 5, 2],
-              backgroundColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-              ],
-              
-              borderWidth: 1
-            }
-          ]
-        },
-        options: {
-          scales: {
-            
-          }
-        }
-      });
